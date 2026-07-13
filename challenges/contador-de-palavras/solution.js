788c148d19cb6698f1b73a1ec93c0e82967728a2
Fernanda <munizsilvestrefernanda@gmail.com>
2026-07-13 16:55:56 -0300

    Foi criada a solução do problema  utilizando o método split para separar as palavras da frase, o loop for para percorrer o array de palavras e verificar se o item já existia ou não.

---
+++ challenges/contador-de-palavras/solution.js
@@ -0,0 +1,21 @@
+const frase =
+  "Este é um exercício de lógica com JavaScript , JavaScript é uma excelente linguagem!"
+    .toLowerCase()
+    .replace(/[,!]/g, "")
+    .trim();
+
+const contador = {};
+
+const palavras = frase.split(" ");
+
+console.log(palavras);
+
+for (const palavra of palavras) {
+  if (Object.hasOwn(contador, palavra) === false) {
+    contador[palavra] = 1;
+  } else {
+    contador[palavra] += 1;
+  }
+}
+
+console.log(contador);
